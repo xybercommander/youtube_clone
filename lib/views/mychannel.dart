9e241bc0067a6d87544113d1de65b16e248872c6
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-22 20:44:16 +0530

    added pic

--- lib/views/mychannel.dart
+++ lib/views/mychannel.dart
@@ -9,8 +9,45 @@ class _MyChannelState extends State<MyChannel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("My Channel"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(57),
+        child: AppBar(
+          iconTheme: IconThemeData(
+            color: Colors.grey[600]
+          ),
+          backgroundColor: Colors.white,
+          title: Text("Channel Name", style: TextStyle(color: Colors.grey[500]),),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: Image.asset("assets/MenuDark.png", height: 20, width: 25,),
+              onPressed: () {
+                print("object");
+              },
+            )
+          ],
+        ),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/Group184.png", height: 35, width: 35,),
+            title: Container(width: 2,height: 2)
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/Group157.png", height: 35, width: 35,),
+            title: Container(width: 2,height: 2)
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset("assets/Group158.png", height: 35, width: 35,),
+            title: Container(width: 2,height: 2)
+          ),
+        ],
+        onTap: (value) {
+          
+        },
       ),
     );
   }
