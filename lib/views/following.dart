949f34fc7857c17b8604f1809a439f2fe97bc92a
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-07-22 17:11:33 +0530

    added many things

---
+++ lib/views/following.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FollowingPage extends StatefulWidget {
+  @override
+  _FollowingPageState createState() => _FollowingPageState();
+}
+
+class _FollowingPageState extends State<FollowingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Following"),
+      ),
+    );
+  }
+}
